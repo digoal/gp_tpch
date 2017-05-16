7448a9f277b7f1fde65cc865d302d3e056892691
Digoal.zhou <digoal@126.com>
2017-05-16 23:41:38 +0800

    Update 20.sql

--- dss/templates/20.sql
+++ dss/templates/20.sql
@@ -25,7 +25,7 @@ where
 					lineitem
 				where
 					l_shipdate >= date ':2'
-					and l_shipdate < date ':2' + interval '1' year
+					and l_shipdate < date ':2' + interval '1 year'
 				group by
 					l_partkey,
 					l_suppkey
